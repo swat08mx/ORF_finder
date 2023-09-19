a22ffead8e1a9488930ed245545b3de03ff63d2f
Swattik Biswas <swattik881@gmail.com>
2023-09-19 14:59:35 +0530

    Updated

--- ORFmod.py
+++ ORFmod.py
@@ -11,7 +11,7 @@ codons = {
 }
 ### To put your own FASTA file into the prog, de-comment the following code and comment the 24th and 25th line ###
 
-file = open('seven.fasta','r')
+file = open('seve.fasta','r')
 newlist=[]
 seq=''
 listtwo=[]
